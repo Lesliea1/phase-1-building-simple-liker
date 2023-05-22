aa200ee5c6bbdbc14985abaf927e9cd13fb2efda
leslie <alvarado.leslie12@yahoo.com>
2023-05-22 12:41:03 -0700

    lab

--- main.js
+++ main.js
@@ -3,8 +3,25 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+ const heart = document.querySelector ('.like')
+ heart.addEventListener('click', function() {
+  mimicServerCall()
+  .then(function(response){
+    if (heart.classList.contains('activated-heart')) {
+      heart.classList.remove('activated-heart');
+    }
+  })
 
 
+    .catch(function(error) {
+      errorModal.classList.remove('hidden');
+      const errorMessage = errorModal.querySelector('p');
+      errorMessage.textContent = error;
+      setTimeout(function() {
+        errorModal.classList.add('hidden');
+     }, 3000)
+    });
+ });
 
 
 //------------------------------------------------------------------------------
